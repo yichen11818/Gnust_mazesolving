8574875aa01cb90341b8177d97d7d5fa98940a51
yichen11818 <1181806270@qq.com>
2024-01-09 11:59:40 +0800

    update c

--- maze.cpp
+++ maze.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 const int MAXLength = 100;  //MAXLength 最大迷宫尺寸
-int maze[MAXLength][MAXLength]; //maze 迷宫本身 1代表墙壁 0代表通路
-int path[MAXLength][MAXLength]; //path 标记已经访问过的路径
+int maze[MAXLength][MAXLength]; //迷宫数组：1代表墙壁，0代表通路
+int path[MAXLength][MAXLength]; //路径数组：标记已经访问过的路径
 int m, n;  //m n 迷宫的行数和列数 
 
-vector<pair<int, int>> solution; // 保存路径的向量
+
 
 // 8个可能的移动方向
 int dir[8][2] = {
@@ -39,7 +38,6 @@ bool isValid(int x, int y) {
 bool dfs(int x, int y) {
     // 如果到达出口
     if (x == m - 1 && y == n - 1) {
-        solution.push_back({x, y}); // 添加出口坐标到路径
         path[x][y] = 8; // 标记路径
         return true;
     }
@@ -52,31 +50,30 @@ bool dfs(int x, int y) {
         // 如果下一个位置有效
         if (isValid(nextX, nextY)) {
             path[x][y] = 8; // 标记路径
-            solution.push_back({x, y}); // 添加当前坐标到路径
 
             if (dfs(nextX, nextY)) // 递归搜索
                 return true;
             else {
                 // 如果没有找到解决方案，回溯
                 path[x][y] = 0;
-                solution.pop_back();
             }
         }
     }
     return false;
 }
 
-//打印迷宫
-void printMaze() {
+
+void printPath() {
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
-            cout << maze[i][j] << " ";
+            if (path[i][j] == 8)
+                cout << "(" << i << ", " << j << ") ";
         }
-        cout << endl;
     }
+    cout << endl;
 }
-//打印路径
-void printPath() {
+
+void printMazeWithPath() {
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
             cout << path[i][j] << " ";
@@ -85,22 +82,11 @@ void printPath() {
     }
 }
 
-/*入口为左上角(0,0),出口为右下角(m-1,n-1)*/
-//模拟输入如下
-/* 
-5 5
-0 0 1 1 1
-1 0 0 0 1
-1 0 1 0 1
-1 0 1 0 0
-1 1 1 1 0 
-*/
 int main() {
     // 输入迷宫大小
     cin >> m >> n;
     
     // 输入迷宫数据
-
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
             cin >> maze[i][j];
@@ -110,11 +96,8 @@ int main() {
 
     // 从入口开始搜索
     if (dfs(0, 0)) {
+        printMazeWithPath(); // 打印带路径的迷宫
         printPath(); // 打印路径
-        for (auto& p : solution) {
-            cout << "(" << p.first << ", " << p.second << ") ";
-        }
-        cout << endl;
     } else {
         cout << "No path found." << endl;
     }
